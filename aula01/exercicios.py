a6135d2a58a0ffca60c03723ba71b23126d8126e
migalha <mastersensi.5421@gmail.com>
2026-09-09 11:51:25 +0000

    aula 01 incompleta

--- aula01/exercicios.py
+++ aula01/exercicios.py
@@ -6,30 +6,53 @@ Escreva sua solucao no lugar do 'pass'.
 
 
 def soma_lista(lista):
-    """Devolve a soma de todos os numeros da lista. Lista vazia devolve 0."""
-    pass
+    soma = 0
+    for n in lista:
+        soma = soma + n
+    return soma
 
 
 def conta_pares(lista):
-    """Devolve quantos numeros da lista sao pares."""
-    pass
+    conta_pares = 0
+    for n in lista:
+        if n % 2 == 0:
+            conta_pares += 1
+    return conta_pares
 
 
 def maior_valor(lista):
-    """Devolve o maior numero da lista. A lista nao esta vazia."""
-    pass
+    maior = lista[0]
+    for n in lista:
+        if maior > n:
+            maior = n
+    return maior    
 
 
 def existe(lista, alvo):
-    """Devolve True se o alvo esta na lista, False se nao esta."""
-    pass
+    for n in lista:
+        return True
+    return False
 
 
 def busca_linear(lista, alvo):
-    """Devolve a posicao do alvo na lista, ou -1 se ele nao estiver."""
-    pass
+    for i in range(len(lista)):
+        if lista[i] == alvo:
+            return i
+    return -1 
 
 
 def segundo_maior(lista):
-    """(Desafio) Devolve o segundo maior, percorrendo a lista uma unica vez."""
-    pass
+    if len(lista) < 2:
+        return None
+
+    maior = float('-inf')
+    segundo = float('-inf')
+
+    for numero in lista:
+        if numero > maior:
+            segundo = maior
+            maior = numero
+        elif numero > segundo and numero != maior:
+            segundo = numero
+
+    return segundo if segundo != float('-inf') else None
